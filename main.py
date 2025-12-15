2a84f5797e4960c1a13d5e00ba68e5003cc43e1e
matutecott <mateortizarnodo@gmail.com>
2025-12-15 15:39:32 +0100

    .py mejorado

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-def suma(a,b):
-    return a+b
 
-a=66
-b=1
+def suma(A,B):
+    """
+    Docstring for suma
+    
+    :param A: 66
+    :param B: 1
+    :return: Suma de A y B
+    """
 
-print(suma(a,b))
+    return A+B
+
+A=66
+B=1
+
+print(suma(A,B))
